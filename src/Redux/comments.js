0372650e769fc4d2519d0b24a3c94aa326ee29ef
Kriti Agarwal <kritiagarwal2098@gmail.com>
2020-09-29 00:25:21 +0530

    Combining Reducers
    Signed-off-by: Kriti Agarwal <kritiagarwal2098@gmail.com>

---
+++ src/Redux/comments.js
@@ -0,0 +1,8 @@
+import { COMMENTS } from '../Data/comments';
+
+export const Comments = (state = COMMENTS , action) => {
+    switch(action.type) {
+        default:
+            return state;
+    }
+}
