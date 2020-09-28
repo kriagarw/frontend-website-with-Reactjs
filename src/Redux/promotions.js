72b4d2dbf53428a2f6d7ef3730a10fae6fb0d8b2
Kriti Agarwal <kritiagarwal2098@gmail.com>
2020-09-29 00:25:55 +0530

    Combining reducers
    Signed-off-by: Kriti Agarwal <kritiagarwal2098@gmail.com>

---
+++ src/Redux/promotions.js
@@ -0,0 +1,8 @@
+import { PROMOTIONS } from '../Data/promotions';
+
+export const Promotions = (state = PROMOTIONS , action) => {
+    switch(action.type) {
+        default:
+            return state;
+    }
+}
